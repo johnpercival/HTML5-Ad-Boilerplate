76199e28fa7b5e902ef54f04e282582d5144438a
John Percival <johnpercival13@gmail.com>
2013-03-08 17:15:02 -0500

    Added Page Visibility API and various clean up
    * Page Visibility API
    * Updated to Prototype to support multiple ads
    * Updated index.html to include the script (may move this into its own
    js file)
    * Added Events for Ad ready, visible, hidden, expand, closed
    * Misc. enhancements to CSS and responsive ad layout

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,12 @@
 ### 0.1.0 (January 25, 2013)
 
 * Initial commit.
+
+### 0.1.1 (March 8, 2013)
+
+* Page Visibility API
+* Updated to Prototype to support multiple ads
+* Updated index.html to include the script (may move this into its own js file)
+* Added Events for Ad ready, visible, hidden, expand, closed
+* Misc. enhancements to CSS and responsive ad layout
+
--- js/adMain.js
+++ js/adMain.js
@@ -1,283 +1,201 @@
-var Ad = {
+/*
+ * HTML5 Ad Object
+ */
 
-  adVar1: 'foo',
-  adVar2: 'bar',
-  adWidth: '',
-  adHeight: '',
-  interactionAction: 'click' || 'touchend',
-  online: navigator.onLine,
-  DNT: navigator.doNotTrack,
-  isMobile: navigator.userAgent.toLowerCase().indexOf('mobile'),
-  isAndroid: navigator.userAgent.toLowerCase().indexOf('android') > -1,
-  publisher: window.location.hostname,
-  mraid: false,
-  safeFrame: false,
+function HTML5Ad () {};
 
-  // The one function that will get called to kick things off - NOTE: Try to keep the initial load around 75-100k
+var proto = {
 
-  init: function(params) {
-    var mainScope = this;//get reference to AD object
+    version: '1.0',
+    author: 'John Percival',
+    adVar1: 'foo',
+    adVar2: 'bar',
+    adWidth: '',
+    adHeight: '',
+    online: navigator.onLine,
+    DNT: navigator.doNotTrack,
+    acceptsCookies: navigator.cookieEnabled,
+    isMobile: navigator.userAgent.toLowerCase().indexOf('mobile'),
+    isAndroid: navigator.userAgent.toLowerCase().indexOf('android') > -1,
+    publisher: window.location.hostname,
+    mraid: false,
+    safeFrame: false,
+    hidden: '',
 
-    //pixel density check
-    console.log('Device Pixel Density: ' + mainScope.getDevicePixelRatio());
+    // The init function that will get called to kick things off - NOTE: Try to keep the initial load around 75-100k
 
-    //get device orientation
-    window.onorientationchange = mainScope.setOrientation;
+    init: function(params) {
+      var mainScope = this;//get reference to AD object
+      var container = params.adContainer,
+          ad = document.querySelector(container),
+          adReady = new CustomEvent('adReady');
 
-    //need location?
-    if(navigator.geolocation) {
-      mainScope.getUserLocation();
-    } else {
-      console.warn('Browser does not support geolocation')
+      //create ad border
+      if (params.createAdBorder) {
+        var adW = window.getComputedStyle(ad,null).getPropertyValue('width');
+        var adH = window.getComputedStyle(ad,null).getPropertyValue('height');
+        ad.style.border = '1px solid #000';
+        ad.style.width = adW;
+        ad.style.height = adH;
+      }
+
+      //IAB's Rich Media API's (MRAID & SAFEFRAMES)
+      if (params.mraid) {
+        mainScope.mraid = params.mraid;
+        var mScript = document.createElement('script');
+        mScript.type = 'text/javascript';
+        mScript.src = 'js/mraidClient.js';
+        document.getElementsByTagName('head')[0].appendChild(mScript);
+        window.addEventListener('doneLoading', console.log('MRAID API Installed'), false);
+      } else if(params.safeFrame) {
+        //IAB safe frame support coming soon using post message to communicate to the pub page iFrame
+        console.log('SafeFrames API installed');
+      }
+
+      if (params.adType === 'static') {
+        console.log('Ad is an static');
+      } else if (params.adType === 'expandable') {
+        console.log('Ad is an expandable');
+      } else {
+        console.log('Ad is an interstital');
+      }
+
+      //pixel density check
+      console.log('Device Pixel Density: ' + mainScope.getDevicePixelRatio());
+
+      //get device orientation
+      window.onorientationchange = mainScope.setOrientation;
+
+      //detect page visibility
+      var visibilityChange;
+      if (typeof document.hidden !== 'undefined') {
+        mainScope.hidden = 'hidden';
+        visibilityChange = 'visibilitychange';
+      } else if (typeof document.mozHidden !== 'undefined') {
+        mainScope.hidden = 'mozHidden';
+        visibilityChange = 'mozvisibilitychange';
+      } else if (typeof document.msHidden !== 'undefined') {
+        mainScope.hidden = 'msHidden';
+        visibilityChange = 'msvisibilitychange';
+      } else if (typeof document.webkitHidden !== 'undefined') {
+        mainScope.hidden = 'webkitHidden';
+        visibilityChange = 'webkitvisibilitychange';
+      }
+      if (typeof document.addEventListener === 'undefined' || typeof mainScope.hidden === 'undefined') {
+        console.warn('This demo requires a browser such as Google Chrome that supports the Page Visibility API.');
+      } else {
+         document.addEventListener(visibilityChange, mainScope.handleVisibilityChange, false);
+      }
+
+      console.log(adW + ' ' +  adH + ' ad on ' + mainScope.publisher + ' with DNT set to ' + mainScope.DNT);
+
+      window.dispatchEvent(adReady);
+    },
+
+
+    //Layout
+    //-------------------------------
+    setOrientation: function(o) {
+      var orientation = window.orientation;
+      if (orientation != orientation1) {
+          switch (orientation) {
+            case 0:
+                console.log('makePortrait');
+                break;
+            case 90:
+                console.log('makeLandscape');
+                break;
+            case -90:
+                console.log('makeLandscape');
+                break;
+            case 180:
+                console.log('makePortrait');
+                break;
+          }
+      }
+      orientation1 = orientation;
+      console.log('o' + orientation)
+    },
+
+    //Utils
+    //-------------------------------
+
+    // Query the device pixel ratio.
+    getDevicePixelRatio: function () {
+      if(window.devicePixelRatio === undefined) {
+        return 1; // No pixel ratio available. Assume 1:1.
+      } else {
+        return window.devicePixelRatio;
+      }
+    },
+
+    handleVisibilityChange: function (event) {
+      var adVisible = new CustomEvent('adVisible'),
+          adHidden = new CustomEvent('adHidden');
+
+      var state = event.target.visibilityState ||
+                  event.target.webkitVisibilityState ||
+                  event.target.mozkitVisibilityState ||
+                  event.target.mskitVisibilityState ||
+                  event.target.oVisibilityState;
+
+      if (state === 'hidden') {
+          console.warn('Pause Ad Rendering');
+          window.dispatchEvent(adHidden);
+      } else {
+          console.warn('Resume Ad Rendering');
+          window.dispatchEvent(adVisible);
+      }
+    },
+
+    //Rich Media Ad Handlers
+    //-------------------------------
+
+    /*
+    *Method for the ads click through destination
+    */
+    openURL: function (url) {
+      if(this.mraid) {
+        mraid.launchURL(url);
+      } else {
+        window.open(url);
+      }
+    },
+
+    expandAd: function() {
+      if(this.mraid) {
+        mraid.expandAd(this.adWidth, this.adHeight);
+      }
+
+      console.log('Ad Expand');
+      toggleLayer('bannerDiv', 'panelDiv');
+
+      var adExpand = new CustomEvent('adExpand');
+      window.dispatchEvent(adExpand);
+    },
+
+    closeAd: function() {
+      if(this.mraid) {
+        mraid.closeAd();
+      }
+
+      console.log('Ad Closed');
+      toggleLayer('panelDiv', 'bannerDiv');
+
+      var adClosed = new CustomEvent('adClosed');
+      window.dispatchEvent(adClosed);
+    },
+
+    toggleLayer: function(fromLayer, toLayer) {
+      var fromElem, toElem, fromElemStyle, toElemStyle;
+
+      fromElem = document.querySelector(fromLayer);
+      fromElem.style.visibility = 'hidden';
+
+      toElem = document.querySelector(toLayer);
+      toElem.style.visibility = 'visible';
     }
-
-    //create ad border (optional)
-    var ad = document.querySelector('.adContainer');
-    var adW = window.getComputedStyle(ad,null).getPropertyValue('width');
-    var adH = window.getComputedStyle(ad,null).getPropertyValue('height');
-    ad.style.border = '1px solid #000';
-    ad.style.width = adW;
-    ad.style.height = adH;
-
-
-    //detect page visibility - sam dutton (google)
-    var hidden, visibilityChange;
-    if (typeof document.hidden !== "undefined") {
-      hidden = "hidden";
-      visibilityChange = "visibilitychange";
-    } else if (typeof document.mozHidden !== "undefined") {
-      hidden = "mozHidden";
-      visibilityChange = "mozvisibilitychange";
-    } else if (typeof document.msHidden !== "undefined") {
-      hidden = "msHidden";
-      visibilityChange = "msvisibilitychange";
-    } else if (typeof document.webkitHidden !== "undefined") {
-      hidden = "webkitHidden";
-      visibilityChange = "webkitvisibilitychange";
-    }
-    if (typeof document.addEventListener === "undefined" || typeof hidden === "undefined") {
-      console.warn("This demo requires a browser such as Google Chrome that supports the Page Visibility API.");
-    } else {
-       document.addEventListener(visibilityChange, mainScope.handleVisibilityChange, false);
-    }
-
-    //accelerometer
-    if (window.DeviceOrientationEvent || window.OrientationEvent) {
-      mainScope.tiltHandler();
-    } else {
-      console.warn('Browser does not support orientation');
-    }
-
-    //IAB's Rich Media API's (MRAID & SAFEFRAMES)
-    if (params.mraid) {
-      mainScope.mraid = params.mraid;
-      var mScript = document.createElement('script');
-      mScript.type = 'text/javascript';
-      mScript.src = 'js/mraidClient.js';
-      document.getElementsByTagName('head')[0].appendChild(mScript);
-
-      window.addEventListener('doneLoading', console.log('MRAID API Installed'), false);
-    } else if(params.safeFrame) {
-      //IAB safe frame support coming soon.
-      //post message to communicate to the pub page iFrame
-      console.log('SafeFrames API installed');
-    }
-
-    if (params.expand) {
-      console.log('Ad is an expand');
-      mainScope.bindExpandActions();
-    } else {
-      console.log('Ad is an interstital');
-      //wire up global click interaction for ad
-      ad.addEventListener(mainScope.interactionAction, function () {
-        var url = document.querySelector('body').getAttribute('data-clickTag');
-        mainScope.openURL(url);
-      }, false);
-
-    }
-
-    console.log(adW + ' ' +  adH + ' ad on ' + mainScope.publisher + ' with DNT set to ' + mainScope.DNT);
-
-  },
-
-
-
-  //Interactions
-  //-------------------------------
-
-  bindExpandActions: function() {
-    //buttons for rich media expand
-    var adButtons = document.querySelectorAll('button');
-    var action = this.interactionAction;
-    for (var i = 0; i < adButtons.length; i++) {
-      adButtons[i].removeAttribute('hidden');
-      adButtons[i].addEventListener(action, this.buttonHandler(event), false);
-    }
-
-  },
-
-  buttonHandler: function(event) {
-    console.log(event)
-  },
-
-  tiltHandler: function(event) {
-    var mainScope = this;
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', function (eventData) {
-          // gamma is the left-to-right tilt in degrees, where right is positive
-          var tiltLR = eventData.gamma;
-          // beta is the front-to-back tilt in degrees, where front is positive
-          var tiltFB = eventData.beta;
-          // alpha is the compass direction the device is facing in degrees
-          var dir = eventData.alpha
-          // deviceorientation does not provide this data
-          var motUD = null;
-          // call our orientation event handler
-          mainScope.deviceOrientationHandler(tiltLR, tiltFB, dir, motUD);
-        }, false);
-    } else if (window.OrientationEvent) {
-        window.addEventListener('MozOrientation', function (eventData) {
-          // x is the left-to-right tilt from -1 to +1, so we need to convert to degress
-          var tiltLR = eventData.x * 90;
-          // y is the front-to-back tilt from -1 to +1, so we need to convert to degress
-          // We also need to invert the value so tilting the device towards us (forward)
-          // results in a positive value.
-          var tiltFB = eventData.y * -90;
-          // MozOrientation does not provide this data
-          var dir = null;
-          // z is the vertical acceleration of the device
-          var motUD = eventData.z;
-          mainScope.deviceOrientationHandler(tiltLR, tiltFB, dir, motUD);
-        }, false);
-    }
-  },
-
-
-
-  //Layout
-  //-------------------------------
-
-  deviceOrientationHandler: function(tiltLR, tiltFB, dir, motionUD) {
-    // Apply the transform to the dummy image
-    var vid = document.querySelector('body');
-    vid.style.webkitTransform = 'rotate(' + tiltLR + 'deg) rotate3d(1,0,0, ' + (tiltFB * -1) + 'deg)';
-    vid.style.MozTransform = 'rotate(' + tiltLR + 'deg)';
-    vid.style.transform = 'rotate(' + tiltLR + 'deg) rotate3d(1,0,0, ' + (tiltFB * -1) + 'deg)';
-  },
-
-  setOrientation: function(o) {
-    var orientation = window.orientation;
-    if (orientation != orientation1) {
-        switch (orientation) {
-          case 0:
-              console.log('makePortrait');
-              break;
-          case 90:
-              console.log('makeLandscape');
-              break;
-          case -90:
-              console.log('makeLandscape');
-              break;
-          case 180:
-              console.log('makePortrait');
-              break;
-        }
-    }
-    orientation1 = orientation;
-    console.log('o' + orientation)
-  },
-
-
-  //Rich Media Ad Handlers
-  //-------------------------------
-
-  openURL: function (url) {
-    if(this.mraid) {
-      mraid.launchURL(url);
-    } else {
-      window.open(url);
-    }
-  },
-
-  expandAd: function() {
-    if(this.mraid) {
-      mraid.expandAd(this.adWidth, this.adHeight);
-    }
-    console.log('Ad Expand');
-    toggleLayer('bannerDiv', 'panelDiv');
-  },
-
-  closeAd: function() {
-    if(this.mraid) {
-      mraid.closeAd();
-    }
-    console.log('Ad Closed');
-    toggleLayer('panelDiv', 'bannerDiv');
-  },
-
-  toggleLayer: function(fromLayer, toLayer) {
-    var fromElem, toElem, fromElemStyle, toElemStyle;
-
-    fromElem = document.querySelector(fromLayer);
-    fromElem.style.display = 'none';
-
-    toElem = document.querySelector(toLayer);
-    toElem.style.display = 'block';
-  },
-
-
-  //Location
-  //-------------------------------
-
-  getUserLocation: function() {
-    // The one function that will get called to kick things off
-    navigator.geolocation.getCurrentPosition(this.handleGeolocationQuery, this.handleLocationErrors);
-  },
-
-  handleLocationErrors: function (error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-          console.error('Error :>> user did not share geolocation data');
-          break;
-      case error.POSITION_UNAVAILABLE:
-          console.error('Error :>> could not detect current position');
-          break;
-      case error.TIMEOUT:
-          console.error('Error :>> retrieving position timed out');
-          break;
-      default:
-          console.error('Error :>> unknown error');
-          break;
-    }
-  },
-
-  handleGeolocationQuery: function(position) {
-    myLat = position.coords.latitude;
-    myLong = position.coords.longitude;
-    console.log('Lat = ' + myLat + ' Long = ' + myLong);
-
-    //do something with the location data here
-  },
-
-
-  //Utils
-  //-------------------------------
-
-  // Query the device pixel ratio.
-  getDevicePixelRatio: function () {
-    if(window.devicePixelRatio === undefined) {
-      return 1; // No pixel ratio available. Assume 1:1.
-    } else {
-      return window.devicePixelRatio;
-    }
-  },
-
-  handleVisibilityChange: function () {
-    if (document[this.hidden]) {
-      console.warn('Pause Ad Rendering');
-    } else {
-      console.warn('Resume Ad Rendering');
-    }
-  }
 
 };
+
+HTML5Ad.prototype = proto;
